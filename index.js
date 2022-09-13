a8d575f233bf5b6865a89cb412b79676f003d383
Tommy Moncaco <103176871+TommyKila@users.noreply.github.com>
2022-09-13 17:03:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,37 +4,76 @@ const inputField = document.querySelectorAll('.input-field');
 const realtimeText = document.querySelectorAll('.realtime-text');
 const completeState = document.querySelector('.complete-state');
 const resetBtn = document.querySelector('.reset-btn');
+const numberInput = document.getElementById('card-number');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+const nameInput = document.getElementById('cardholder-name');
+const cvcInput = document.getElementById('cvc');
 let count = 0;
+let numberPattern = /^(?:[0-9]{4}) (?:[0-9]{4}) (?:[0-9]{4}) (?:[0-9]{4})$/;
+let alphCheck = /[a-zA-Z]/
 
-resetBtn.addEventListener('click', function (e) {
+const defTextArr = Array.from(realtimeText)
+
+const defValue = defTextArr.reduce(function (acc, text) {
+    return acc.concat(text.textContent);
+}, []);
+
+resetBtn.addEventListener('click', function () {
     inputField.forEach(function (field) {
         field.value = "";
     });
     realtimeText.forEach(function (text, index) {
         text.textContent = defValue[index];
     });
-    form.style.display ="block";
+    form.style.display = "block";
     completeState.classList.remove("complete-message");
 });
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+
     inputField.forEach(function (field) {
-        const fieldId = field.id;
+        checkBlank(field);
+    });
+
+    if (!numberPattern.test(numberInput.value) && numberInput.value !== "") {
+        numberInput.parentElement.classList.add("wrong-format");
+    } else {
+        numberInput.parentElement.classList.remove("wrong-format");
+    }
+
+    if (monthInput.value > 12 || monthInput.value < 1) {
+        monthInput.parentElement.classList.add("wrong-month");
+    } else {
+        monthInput.parentElement.classList.remove("wrong-month");
+    }
+
+    checkAlp(monthInput);
+    checkAlp(yearInput);
+    checkAlp(cvcInput);
+
+    if (yearInput.value < 1 && yearInput.value !== "") {
+        yearInput.parentElement.classList.add("wrong-year");
+    } else {
+        yearInput.parentElement.classList.remove("wrong-year");
+    }
+
+    if (cvcInput.value.length !== 3 && cvcInput.value !== "") {
+        cvcInput.parentElement.classList.add("wrong-format");
+    } else {
+        cvcInput.parentElement.classList.remove("wrong-format");
+    }
+
+
+    inputField.forEach(function (field) {
         const fieldParent = field.parentElement;
-        const fieldGrandparent = fieldParent.parentElement;
-        if (fieldId === "cardholder-name") {
-            checkBlank(field)
-        } else {
-            checkBlank(field);
-            checkFormat(field);
-        }
         if (!fieldParent.classList.contains("wrong-format") && !fieldParent.classList.contains("blank-alert") &&
-            !fieldGrandparent.classList.contains("wrong-format") && !fieldGrandparent.classList.contains("blank-alert")) {
+            !fieldParent.classList.contains("wrong-month")) {
             count++;
         }
-        console.log(count);
     });
+
     if (count === 5) {
         form.style.display = "none";
         completeState.classList.add("complete-message");
@@ -43,43 +82,21 @@ form.addEventListener('submit', function (e) {
 })
 
 function checkBlank(item) {
-    if (item.id === "month" || item.id === "year") {
-        if (item.value === "") {
-            item.parentElement.parentElement.classList.add("blank-alert");
-        } else {
-            item.parentElement.parentElement.classList.remove("blank-alert");
-        }
+    if (item.value === "") {
+        item.parentElement.classList.add("blank-alert");
     } else {
-        if (item.value === "") {
-            item.parentElement.classList.add("blank-alert");
-        } else {
-            item.parentElement.classList.remove("blank-alert");
-        }
-    }
-
-}
-
-function checkFormat(item) {
-    if (item.id === "month" || item.id === "year") {
-        if (isNaN(item.value)) {
-            item.parentElement.parentElement.classList.add("wrong-format");
-        } else {
-            item.parentElement.parentElement.classList.remove("wrong-format");
-        }
-    } else {
-        if (isNaN(item.value)) {
-            item.parentElement.classList.add("wrong-format");
-        } else {
-            item.parentElement.classList.remove("wrong-format");
-        }
+        item.parentElement.classList.remove("blank-alert");
     }
 }
 
-const defTextArr = Array.from(realtimeText)
+function checkAlp(item) {
+    if (alphCheck.test(item.value)) {
+        item.parentElement.classList.add("wrong-format");
+    } else {
+        item.parentElement.classList.remove("wrong-format");
+    }
+}
 
-const defValue = defTextArr.reduce(function (acc, text) {
-    return acc.concat(text.textContent);
-}, []);
 
 inputField.forEach(function (field) {
     field.addEventListener('input', function (e) {
@@ -87,11 +104,12 @@ inputField.forEach(function (field) {
         const fieldValue = e.currentTarget.value;
         realtimeText.forEach(function (value, index) {
             const valueId = value.dataset.id;
-            if (valueId === inputId) {
-                if (inputId === "card-number") {
-                    value.textContent = fieldValue.match(/.{1,4}/g).join(" ");
-                    if (value.textContent.length === 1) {
-                        value.textContent = defValue[index];
+            if (valueId == inputId) {
+                if (inputId === "month" || inputId === "year") {
+                    if (fieldValue < 10 && fieldValue > 0) {
+                        value.textContent = `0${fieldValue}`;
+                    } else {
+                        value.textContent = fieldValue;
                     }
                 } else {
                     value.textContent = fieldValue;
